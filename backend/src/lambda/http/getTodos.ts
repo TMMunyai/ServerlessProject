705c9310e09f42dba36915152e0ea995e60b61d9
TMMunyai <acumenone.tm@gmail.com>
2022-08-16 10:25:47 +0200

    createTodo func done commit

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,35 +1,30 @@
 import 'source-map-support/register'
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import {CreateTodoRequest} from '../../requests/CreateTodoRequest'
+import {createToDo} from '../../businessLogic/ToDo'
 
-import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
-import { getUserId } from '../utils';
-import { createLogger } from '../../utils/logger'
-import { S3Helper } from '../../helpers/s3Helper'
-import { TodosAccess } from '../../helpers/todosAccess'
-
-const s3Helper = new S3Helper()
-const logger = createLogger('todos')
-
-// TODO: Get all TODO items for a current user
-export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   
-  //skipping auth part for now
-  const userId = '1234' 
-  logger.info(`get groups for user ${userId}`)
-  const result = await new TodosAccess().getUserTodos(userId)
-    
-  for(const recordItem of result){
-      recordItem.attachmentUrl = await s3Helper.getTodoAttachmentUrl(recordItem.todoId)
-  }
+  //print the event received by the lamda func
+  //check authorization status
+  //parse the event data received into newTodo variable of type CreateTodoRequest interface
+  //return results upon completion
+
+  console.log("Processing Event ", event);
+  const authorization = event.headers.Authorization;
+  const splitAuth = authorization.split(' ');
+  const jwtToken = splitAuth[1];
+
+  const newTodo: CreateTodoRequest = JSON.parse(event.body);
+  const toDoItem = await createToDo(newTodo, jwtToken);
 
   return {
-    statusCode: 200,
+    statusCode: 201,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*',
     },
     body: JSON.stringify({
-      items:result
-    })
-    }
-  }
+        'item': toDoItem
+    }),
+}
+};

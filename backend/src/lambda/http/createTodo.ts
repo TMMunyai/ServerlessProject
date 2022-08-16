3165398e9742c262d79328e45f5ab8c1443c7f83
TMMunyai <acumenone.tm@gmail.com>
2022-08-16 10:26:21 +0200

    createTodo Func done

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,30 +1,30 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult,APIGatewayProxyHandler } from 'aws-lambda'
 import 'source-map-support/register'
-//import * as middy from 'middy'
-//import { cors } from 'middy/middlewares'
-import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import { getUserId } from '../utils';
-import { createTodo } from '../../businessLogic/todos'
-import { TodosAccess } from '../../helpers/todosAccess'
-import { createLogger } from '../../utils/logger'
-
-const logger = createLogger('todos')
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import {CreateTodoRequest} from '../../requests/CreateTodoRequest'
+import {createToDo} from '../../businessLogic/ToDo'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  
+  //print the event received by the lamda func
+  //check authorization status
+  //parse the event data received into newTodo variable of type CreateTodoRequest interface
+  //return results upon completion
 
-  //skipping auth part for now
-  const userId = '1234' 
-  logger.info(`create group for user ${userId} with data ${newTodo}`)
-  const item = await new TodosAccess().CreateTodo(newTodo,userId)
+  console.log("Processing Event ", event);
+  const authorization = event.headers.Authorization;
+  const splitAuth = authorization.split(' ');
+  const jwtToken = splitAuth[1];
+
+  const newTodo: CreateTodoRequest = JSON.parse(event.body);
+  const toDoItem = await createToDo(newTodo, jwtToken);
 
   return {
     statusCode: 201,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*',
     },
     body: JSON.stringify({
-      item:item
-    })
-    }
-  }
+        'item': toDoItem
+    }),
+}
+};

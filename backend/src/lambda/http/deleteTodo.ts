0633c4b10f839f75e40ffca0c3f3e93d7a3cef46
TMMunyai <acumenone.tm@gmail.com>
2022-08-16 11:29:55 +0200

    http functions all done

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,68 +1,28 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-import { deleteTodo } from '../../businessLogic/todos'
-import { getUserId } from '../utils'
-import { createLogger } from '../../utils/logger'
-import { TodosAccess } from '../../helpers/todosAccess'
-
-const logger = createLogger('todos')
-const todosAccess = new TodosAccess()
+import {deleteTodo} from '../../businessLogic/ToDo'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  //print the event received by the lamda func
+  //check authorization status -- remove spaces from the header data
+  //get todoId from the event data
+  //call the deteteTodo func to query the item  from the db and delete if found in the db
+
+  console.log('Processing Event ', event)
+  const authorization = event.headers.Authorization
+  const splitAuth = authorization.split(' ')
+  const jwtTokenAuth = splitAuth[1]
+
   const todoId = event.pathParameters.todoId
-  if(!todoId){
-    logger.error('Error!! invalid delete attempt without todo id. Please include todo id')
-    return {        
-      statusCode:400 ,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify({
-        message:'Error! invalid parameters'
-      })
-    }
-  }
 
-  //to implement authorization later
-  const userId = '1234'
-  const item = await todosAccess.getTodoById(todoId)
-  if(item.Count == 0){
-    logger.error(`user ${userId} requesting delete for a todo that do not exist. Id of requested TODO : ${todoId}`)
-    return {
-      statusCode:400 ,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify({
-        //confirm this part
-        message: 'TODO with specified id does not exists'
-      })
-    }
-  }
+  const deletedData = await deleteTodo(todoId, jwtTokenAuth)
 
-  if(item.Items[0].userId !== userId){
-    logger.error(`user ${userId} requesting delete todo created by another user, Todo id ${todoId}`)
-    return {
-      statusCode:400 ,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify({
-          message:'Error! TODO owened by another user'
-    })
-    }
-  }
-
-  logger.info(`User ${userId} deleting todo ${todoId}`)
-  await todosAccess.deleteTodoById(todoId)
   return {
-    statusCode:204 ,
+    statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin': '*',
     },
-    body: JSON.stringify({
-      messgae: 'Item with id ${todoId} deleted successfully'
-    })
+    body: deletedData,
   }
 }

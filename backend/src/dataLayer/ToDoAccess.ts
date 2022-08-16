8ad3bbcfb44cec3c3c50e4896c2b8e7b8b2428af
TMMunyai <acumenone.tm@gmail.com>
2022-08-16 12:32:35 +0200

    datalayer and business logic modules corrected

---
+++ backend/src/dataLayer/ToDoAccess.ts
@@ -0,0 +1,107 @@
+import * as AWS from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+import { TodoItem } from '../models/TodoItem'
+import { TodoUpdate } from '../models/TodoUpdate';
+import { Types } from 'aws-sdk/clients/s3';
+
+
+const AWSXRay = require('aws-xray-sdk')
+
+const TAWS = AWSXRay.captureAWS(AWS)
+
+export class ToDoAccess{ 
+    constructor(
+        private readonly docClient: DocumentClient = new TAWS.DynamoDB.DocumentClient(),
+        private readonly s3Client: Types = new TAWS.S3({ signatureVersion: 'v4' }),
+        private readonly todoTable = process.env.TODOS_TABLE,
+        private readonly s3BucketName = process.env.S3_BUCKET_NAME) {
+    }    
+       
+    async getAllTodo(userId: string): Promise<TodoItem[]>{
+        const params = {
+            TableName: this.todoTable,
+            KeyConditionExpression: "#userId = :userId",
+            ExpressionAttributeNames: {
+                "#userId": "userId"
+            },
+            ExpressionAttributeValues: {
+                ":userId": userId
+            }
+        };
+
+        const result = await this.docClient.query(params).promise();
+        console.log(result);
+        const items = result.Items;
+
+        return items as TodoItem[];
+    }
+
+    async CreateTodo(todoItem: TodoItem): Promise<TodoItem>{
+        const params = {
+            TableName: this.todoTable,
+            Item: todoItem,
+        };
+
+        const result = await this.docClient.put(params).promise();
+        console.log(result);
+
+        return todoItem as TodoItem;
+    }
+
+    async generateUploadUrl(todoId: string): Promise<string> {
+
+        const url = this.s3Client.getSignedUrl('putObject', {
+            Bucket: this.s3BucketName,
+            Key: todoId,
+            Expires: 990,
+        });
+        console.log(url);
+
+        return url as string;
+    }
+
+    async updateTodo(todoUpdate: TodoUpdate, todoId:string, userId: string): Promise<TodoUpdate> {
+        const params = {
+            TableName: this.todoTable,
+            Key: {
+                "userId": userId,
+                "todoId": todoId
+            },
+            UpdateExpression: "set #a = :a, #b = :b, #c = :c",
+            ExpressionAttributeNames: {
+                "#a": "name",
+                "#b": "dueDate",
+                "#c": "done"
+            },
+            ExpressionAttributeValues: {
+                ":a": todoUpdate['name'],
+                ":b": todoUpdate['dueDate'],
+                ":c": todoUpdate['done']
+            },
+            ReturnValues: "ALL_NEW"
+        };
+
+        const result = await this.docClient.update(params).promise();
+        console.log(result);
+        const attributes = result.Attributes;
+
+        return attributes as TodoUpdate;
+    }
+
+    async deleteTodo(todoId: string, userId: string){
+        const params = {
+            TableName: this.todoTable,
+            Key: {
+                "userId": userId,
+                "todoId": todoId
+            },
+        };
+
+        const result = await this.docClient.delete(params).promise();
+        console.log(result);
+
+        return "" as string;
+    }
+
+}
--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,7 +1,7 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-import {deleteTodo} from '../../businessLogic/ToDo'
+import {deleteToDo} from '../../businessLogic/ToDo'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   //print the event received by the lamda func
@@ -16,7 +16,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
 
   const todoId = event.pathParameters.todoId
 
-  const deletedData = await deleteTodo(todoId, jwtTokenAuth)
+  const deletedData = await deleteToDo(todoId, jwtTokenAuth)
 
   return {
     statusCode: 200,
